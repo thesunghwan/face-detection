ebb41916261d10f00286ec036ab1999b1b9fc8f9
thesunghwan <thesunghwan@gmail.com>
2016-11-11 17:54:20 +0900

    svm done

--- code/get_positive_features.m
+++ code/get_positive_features.m
@@ -37,7 +37,6 @@ for n = 1:num_images
     hog = vl_hog(single(positive_image), feature_params.hog_cell_size);
     hog_vec = transpose(hog(:));
     features_pos = [features_pos; hog_vec];
-    disp(n);
 end
 
 % placeholder to be deleted
